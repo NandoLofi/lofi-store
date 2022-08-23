655cbf1e7d01fa94b4920dd7c32cca12d4f28c64
Fernando Galvan <n.brighter00@gmail.com>
2022-08-22 19:17:12 -0500

    have basics of redux and store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import productsReducer from './features/productsSlice';
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
+      
   </React.StrictMode>
   </Router>
 );
