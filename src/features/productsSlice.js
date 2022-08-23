258e667e41e68fd2420770d82a67e8dae0516292
Fernando Galvan <n.brighter00@gmail.com>
2022-08-22 22:50:25 -0500

    adding status and actions

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -1,14 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
 
 
 const initialState = {
     items: [],
     status: null
 }
+export const productsFetch = createAsyncThunk(
+    "products/productsFetch",
+    async ()=> {
+        const response = await axios.get("https://lofi-store.herokuapp.com/products")
+        .then((response) => response.data)
+    }
+)
+
 const productSlice = createSlice({
     name: "products",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [productsFetch.pending]: (state, action) =>{
+            state.status = "pending"
+        },
+        [productsFetch.fulfilled]: (state, action) =>{
+            state.status = "success"
+            state.items = action.payload
+        },
+        [productsFetch.e]: (state, action) =>{
+            state.status = "pending"
+        }
+    }
+        
 })
 
 export default productSlice.reducer
