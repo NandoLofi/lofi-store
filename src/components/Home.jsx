374c4e3e23cc3c9ae1fc32c81e6c2a275ae880cc
Fernando Galvan <n.brighter00@gmail.com>
2022-08-23 09:10:20 -0500

    added home product page and display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import { useGetAllProductsQuery } from '../features/productsApi'
+import "./Home.css"
+import { productsAPI, useGetAllProductsQuery } from '../features/productsApi'
 
 export default function Home() {
   const { data, error, isLoading } = useGetAllProductsQuery()
 
   return (
-    <div>
-        <h2>Home</h2>
+    <div className="home__container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p> An error occured </p>
+      ) : (
+        <>
+          <h2>All Products</h2>
+          <div className="products">
+            {data.map((product) => (
+              <div key={product._id} className="product">
+                <h3>{productsAPI.name}</h3>
+                <img src={product.imageUrl} alt={product.name} />
+                <div className="deta">
+                  <span className="details">{product.description}</span>
+                  <span className="priced">${product.price}</span>
+                </div>
+                <button> Add to Cart </button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
--- src/features/productsApi.js
+++ src/features/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const productsAPI = createApi({
     reducerPath: "productsAPI",
-    baseQuery: fetchBaseQuery({baseUrl:"ttps://lofi-store.herokuapp.com/"}),
+    baseQuery: fetchBaseQuery({baseUrl:"https://lofi-store.herokuapp.com/"}),
     endpoints: (builder)=> ({
         getAllProducts: builder.query({
             query: ()=> "products",
